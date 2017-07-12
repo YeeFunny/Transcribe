a99ac926a8d98225f16efce2ad90475c7c68b421
Yifang Liu <yliu40@email.wm.edu>
2017-07-12 12:39:04 -0400

    New decode

--- encode_debug.py
+++ encode_debug.py
@@ -5,7 +5,9 @@ myConn = MysqlConn('transcribe', 'root', 'root')
 myCur = myConn.cursor()
 
 
-querySQL = """select id, text from element_texts where element_id = '86' and record_type = 'Item';"""
+querySQL = """select id, text from element_texts where element_id in ('1', '86') and record_type = 'Item'
+              and record_id not in ('208', '217', '141', '181', '296', '297', '298', '300', '303', '327',
+              '328', '329');"""
 myCur.execute(querySQL)
 
 for primaryId, text in myCur.fetchall():
@@ -13,7 +15,10 @@ for primaryId, text in myCur.fetchall():
         decodeText = text.encode('cp1252')
     except UnicodeEncodeError:
         decodeText = text.encode('cp1252', 'backslashreplace')
-        decodeText = decodeText.replace('\\x9d', '\x9d').decode('utf8')
+        try:
+            decodeText = decodeText.replace('\\x9d', '\x9d').decode('utf8')
+        except UnicodeDecodeError:
+            print primaryId
     else:
         try:
             decodeText = decodeText.decode('utf8')
